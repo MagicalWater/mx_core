de65076063f44b9755b6d71c1df287a7ca04dd84
MagicalWater <crazydennies@gmail.com>
2020-06-09 14:52:16 +0800

    #3.6.5+1 * UriUtil 工具 - 修正 addParam 方法無效

--- lib/util/uri_util.dart
+++ lib/util/uri_util.dart
@@ -1,50 +1,25 @@
-
 class UriUtil {
 
   UriUtil._();
 
   /// 新加入一個query進入原本的uri
+  /// 若key已存在, 則會進行替代
   static Uri addParams(Uri uri, String key, dynamic value) {
-    final regex = "[\\w]+(?=\\[[0-x9]+\\])";
-    Map<String, dynamic> newParams = {};
-    RegExp regExp = RegExp(regex);
-    uri.queryParameters.forEach((key, value) {
-      String realKey = key;
-      if (regExp.hasMatch(key)) {
-        /// 代表是個陣列
-        realKey = regExp.firstMatch(key).group(0);
-        _addPair(target: newParams, key: realKey, value: [value]);
-      } else {
-        /// 代表並非陣列
-        _addPair(target: newParams, key: realKey, value: value);
-      }
-    });
-    return Uri(scheme: uri.scheme, host: uri.host, path: uri.path, queryParameters: newParams);
-  }
-
-  static void _addPair({Map<String, dynamic> target, String key, dynamic value}) {
-    if (target.containsKey(key)) {
-      var oriValue = target[key];
-      if (value is List<String>) {
-        if (oriValue is String) {
-          /// 原先元素是個字串, 更改為陣列並添加
-          target[key] = [oriValue] + value;
-        } else if (oriValue is List<String>) {
-          /// 原先元素是個陣列, 直接添加
-          target[key] = oriValue + value;
-        }
-      } else if (value is String) {
-        if (oriValue is String) {
-          /// 原先元素是個字串, 直接修改參數
-          target[key] = value;
-        } else if (oriValue is List<String>) {
-          /// 原先元素是個陣列, 直接添加
-          oriValue.add(value);
-          target[key] = oriValue;
-        }
-      }
+    Map<String, dynamic> oriQuery;
+    if (value is List) {
+      oriQuery = Map.from(uri.queryParametersAll);
+      oriQuery[key] = value;
     } else {
-      target[key] = value;
+      oriQuery = Map.from(uri.queryParameters);
+      oriQuery[key] = value;
     }
+
+    return Uri(
+      scheme: uri.scheme,
+      host: uri.host,
+      path: uri.path,
+      port: uri.port,
+      queryParameters: oriQuery,
+    );
   }
 }
