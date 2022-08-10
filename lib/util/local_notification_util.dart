1a948451ecf0f2cac54a50fb1dc34203daafe5f6
Magical Water <crazydennies@gmail.com>
2022-08-10 14:46:20 +0800

    #6.9.6+1 - 移除 flutter_local_notifications 的依賴, 同時註解 LocalNotificationUtil 本地通知工具 (這個應該讓需要使用的專案自行調配)

--- lib/util/local_notification_util.dart
+++ lib/util/local_notification_util.dart
@@ -1,66 +1,66 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-/// 本地推送工具
-class LocalNotificationUtil {
-  LocalNotificationUtil({
-    required String androidIcon,
-  }) {
-    /// 初始化通知設定
-    FlutterLocalNotificationsPlugin plugin = FlutterLocalNotificationsPlugin();
-    var settingsAndroid = AndroidInitializationSettings(androidIcon);
-    var settingsIOS = IOSInitializationSettings(
-      onDidReceiveLocalNotification: onReceiveIOS,
-    );
-    var settings = InitializationSettings(
-      android: settingsAndroid,
-      iOS: settingsIOS,
-    );
-    plugin.initialize(
-      settings,
-      onSelectNotification: onSelectNotification,
-    );
-  }
-
-  /// 當 iOS 版本 < 10.0, 並且應用位於前台時, 在此處可收到通知, 但是並不會顯示在螢幕上, 因此需要手動呼叫
-  Future<dynamic> onReceiveIOS(
-      int id, String? title, String? body, String? payload) async {
-    print("收到前台通知: 標題 - $title, 內容 - $body");
-  }
-
-  /// 當通知被點擊時, 觸發此方法
-  Future<dynamic> onSelectNotification(String? payload) async {
-    print("通知被點擊: payload - ${payload ?? "null"}");
-  }
-
-  /// 顯示一個通知
-  Future<void> showNotification({
-    required int id,
-    required String title,
-    required String content,
-    String? payload,
-  }) {
-    FlutterLocalNotificationsPlugin notificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
-      '12345',
-      '這是通道名稱',
-      '這是通道說明',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-    var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
-    var channelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics,
-      iOS: iOSPlatformChannelSpecifics,
-    );
-//    print("觸發通知開始");
-    return notificationsPlugin.show(
-      id,
-      title,
-      content,
-      channelSpecifics,
-      payload: payload,
-    );
-//    print("觸發通知結束");
-  }
-}
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+//
+// /// 本地推送工具
+// class LocalNotificationUtil {
+//   LocalNotificationUtil({
+//     required String androidIcon,
+//   }) {
+//     /// 初始化通知設定
+//     FlutterLocalNotificationsPlugin plugin = FlutterLocalNotificationsPlugin();
+//     var settingsAndroid = AndroidInitializationSettings(androidIcon);
+//     var settingsIOS = IOSInitializationSettings(
+//       onDidReceiveLocalNotification: onReceiveIOS,
+//     );
+//     var settings = InitializationSettings(
+//       android: settingsAndroid,
+//       iOS: settingsIOS,
+//     );
+//     plugin.initialize(
+//       settings,
+//       onSelectNotification: onSelectNotification,
+//     );
+//   }
+//
+//   /// 當 iOS 版本 < 10.0, 並且應用位於前台時, 在此處可收到通知, 但是並不會顯示在螢幕上, 因此需要手動呼叫
+//   Future<dynamic> onReceiveIOS(
+//       int id, String? title, String? body, String? payload) async {
+//     print("收到前台通知: 標題 - $title, 內容 - $body");
+//   }
+//
+//   /// 當通知被點擊時, 觸發此方法
+//   Future<dynamic> onSelectNotification(String? payload) async {
+//     print("通知被點擊: payload - ${payload ?? "null"}");
+//   }
+//
+//   /// 顯示一個通知
+//   Future<void> showNotification({
+//     required int id,
+//     required String title,
+//     required String content,
+//     String? payload,
+//   }) {
+//     FlutterLocalNotificationsPlugin notificationsPlugin =
+//         FlutterLocalNotificationsPlugin();
+//     var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+//       '12345',
+//       '這是通道名稱',
+//       '這是通道說明',
+//       importance: Importance.max,
+//       priority: Priority.high,
+//     );
+//     var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+//     var channelSpecifics = NotificationDetails(
+//       android: androidPlatformChannelSpecifics,
+//       iOS: iOSPlatformChannelSpecifics,
+//     );
+// //    print("觸發通知開始");
+//     return notificationsPlugin.show(
+//       id,
+//       title,
+//       content,
+//       channelSpecifics,
+//       payload: payload,
+//     );
+// //    print("觸發通知結束");
+//   }
+// }
